94193488b3c926856c25aedb6987bedaa74c4697
nick27willia <nick27willia@gmail.com>
2021-12-03 22:01:29 -0500

    Nothing

--- Project5-copy/WrittenDescription.java
+++ Project5-copy/WrittenDescription.java
@@ -8,35 +8,34 @@
 public class WrittenDescription
 {
  
-    /*
-     * So my program is basically a simple choice based game similar to pokemon
-     * or any other game of the sort. It asks the user for a lot of input and 
-     * it returns a lot of output. It mainly relys on if statements for conditionals,
-     * while loops for battles, an enum and other methods that are called. 
-     * The goal is to make it to the end of the code but if you fail the code
-     * closes and the end is signified by a congrats method.
-     * 
-     * To run the program is simple, compile both project 5 and the Folkemon classes.
-     * Then just enter in inputs as prompted. I will provide a sample below.
-     * 
-     * Nick
-     * two
-     * L
-     * two
-     * two
-     * R
-     * L
-     * two
-     * one
-     * three
-     * two
-     * two
-     * two
-     * 
-     * See all the code is looking for is one word answers that satisfy if or
-     * equals statements mainly. There are a few different paths the user can
-     * end up on so have fun!
-     */
+    
+     // So my program is basically a simple choice based game similar to pokemon
+     // or any other game of the sort. It asks the user for a lot of input and 
+     // it returns a lot of output. It mainly relys on if statements for conditionals,
+     // while loops for battles, an enum and other methods that are called. 
+     // The goal is to make it to the end of the code but if you fail the code
+     // closes and the end is signified by a congrats method.
+     
+     // To run the program is simple, compile both project 5 and the Folkemon classes.
+     // Then just enter in inputs as prompted. I will provide a sample below.
+     
+     // Nick
+     // two
+     // L
+     // two
+     // two
+     // R
+     // L
+     // two
+     // one
+     // three
+     // two
+     // two
+     // two
+     
+     // See all the code is looking for is one word answers that satisfy if or
+     // equals statements mainly. There are a few different paths the user can
+     // end up on so have fun!
     
     
     
